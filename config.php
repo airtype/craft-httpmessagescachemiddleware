677c3f22355206ea64fc1baa3f4e8e8ccd057e16
Wes Rice <wes@airtype.com>
2016-03-27 11:32:30 -0400

    No longer a Craft plugin

--- config.php
+++ config.php
@@ -1,9 +1,15 @@
 <?php
 
-return [
+namespace Craft;
+
+$config = [
 
     'defaultCacheDuration' => 60 * 60, // 1 Hour
 
     'disableCache' => false,
 
 ];
+
+foreach ($config as $key => $value) {
+    craft()->config->set($key, $value, 'httpmessagescachemiddleware');
+}
